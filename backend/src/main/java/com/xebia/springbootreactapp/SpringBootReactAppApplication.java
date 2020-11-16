ca7a84bd56a52b3efa113723110bb6c4eb68fb3d
pbalogh <peter.balogh@vidux.net>
2020-11-16 14:20:49 +0100

    Spring + React + Gradle initialized

---
+++ backend/src/main/java/com/xebia/springbootreactapp/SpringBootReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.springbootreactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactAppApplication.class, args);
+	}
+
+}
