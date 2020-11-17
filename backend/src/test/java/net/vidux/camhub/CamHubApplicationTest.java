c5b499a6864f291e92e561e7f797166d3b6fb806
psiklos <peter.siklos@vidux.net>
2020-11-17 14:09:36 +0100

    [#20] Readded Test

---
+++ backend/src/test/java/net/vidux/camhub/CamHubApplicationTest.java
@@ -0,0 +1,11 @@
+package net.vidux.camhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamHubApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
