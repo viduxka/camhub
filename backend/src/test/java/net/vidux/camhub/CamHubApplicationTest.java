340c0b0251e32b94fd77e8d183e2db16f8ceaacc
psiklos <peter.siklos@vidux.net>
2020-11-17 15:13:26 +0100

    [#20] Disabled test

--- backend/src/test/java/net/vidux/camhub/CamHubApplicationTest.java
+++ backend/src/test/java/net/vidux/camhub/CamHubApplicationTest.java
@@ -1,11 +1,12 @@
 package net.vidux.camhub;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CamHubApplicationTest {
-    @Test
+
+    @Disabled
     void contextLoads() {
     }
 }
