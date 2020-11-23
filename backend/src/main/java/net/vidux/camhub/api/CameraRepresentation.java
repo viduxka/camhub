6bce40ca14d562b4f107c4ee7fa2e7fec15e6ff8
psiklos <peter.siklos@vidux.net>
2020-11-23 17:10:46 +0100

    [#28] Added CameraRepresentation relation values

--- backend/src/main/java/net/vidux/camhub/api/CameraRepresentation.java
+++ backend/src/main/java/net/vidux/camhub/api/CameraRepresentation.java
@@ -2,6 +2,7 @@ package net.vidux.camhub.api;
 
 import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.util.Date;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Relation(collectionRelation = "cameras", itemRelation = "camera")
 @Builder
 public class CameraRepresentation extends RepresentationModel<CameraRepresentation> {
     private String name;
