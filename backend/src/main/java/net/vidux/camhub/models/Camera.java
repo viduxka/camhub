77ec2c3086c6923654aea21209631b59a9773bb5
gbene <gabor.bene@vidux.net>
2020-11-24 14:10:16 +0100

    #28 add CameraRepoInMemory specification

--- backend/src/main/java/net/vidux/camhub/models/Camera.java
+++ backend/src/main/java/net/vidux/camhub/models/Camera.java
@@ -1,14 +1,16 @@
 package net.vidux.camhub.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class Camera {
-    private final String name;
-    private String ip;
-    private String firmware;
-    private Date lastSeen;
+  private final String name;
+  private String ip;
+  private String firmware;
+  private Date lastSeen;
 }
