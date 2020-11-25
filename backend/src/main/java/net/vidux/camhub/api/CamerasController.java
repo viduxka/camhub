f224b69f7ae6f7c75b241c247421f7f1690fc5e5
Gólity <patrik.golity@vidux.net>
2020-11-25 08:29:47 +0100

    [#28] Add CrossOrigin

--- backend/src/main/java/net/vidux/camhub/api/CamerasController.java
+++ backend/src/main/java/net/vidux/camhub/api/CamerasController.java
@@ -7,6 +7,7 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/cameras")
 @RestController
 @Service
+@CrossOrigin
 public class CamerasController {
 
   private final CameraRepo cameraRepo;
