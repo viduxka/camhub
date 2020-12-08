d9bc499701d43dd1023d6ec17f87e0d459dc4cc0
psiklos <peter.siklos@vidux.net>
2020-12-08 14:19:38 +0100

    [#44] Replaced long id with UUID

--- backend/src/main/java/net/vidux/camhub/camera/Camera.java
+++ backend/src/main/java/net/vidux/camhub/camera/Camera.java
@@ -7,11 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,7 +22,12 @@ import java.time.Instant;
 @Entity
 @Builder
 public class Camera {
-  @NonNull @Id @GeneratedValue private long id;
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @Type(type = "uuid-char")
+  @Column(columnDefinition = "varchar(36)", nullable = false, updatable = false)
+  private UUID id;
+
   @NonNull private String name;
   @NonNull private String ip;
   @NonNull private String firmware;
