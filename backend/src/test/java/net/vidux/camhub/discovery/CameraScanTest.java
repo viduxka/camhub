2a5668beef2ba4fa2b8a76d5a51a99e94338af4e
Gólity <patrik.golity@vidux.net>
2020-12-09 16:13:20 +0100

    [#42] Added CameraScanTest

---
+++ backend/src/test/java/net/vidux/camhub/discovery/CameraScanTest.java
@@ -0,0 +1,29 @@
+package net.vidux.camhub.discovery;
+
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CameraScanTest {
+
+  @Test
+  void callViduxHelper() {
+    CameraScan viduxHelper = new CameraScanViduxHelper();
+    try {
+      Set<RawCameraData> set = viduxHelper.scanCams().get();
+      Assertions.assertFalse(set.isEmpty());
+      Assertions.assertEquals(19, set.size());
+
+      for (RawCameraData cam : set) {
+        Assertions.assertNotNull(cam.getName());
+        Assertions.assertNotNull(cam.getFirmware());
+        Assertions.assertNotNull(cam.getIpAddress());
+        Assertions.assertNotNull(cam.getSerialNumber());
+        Assertions.assertEquals(9, cam.getSerialNumber().length());
+      }
+
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+}
