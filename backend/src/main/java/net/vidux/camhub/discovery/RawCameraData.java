d4be6a4ec65161e63fd6cef6866ff0e6dec1eeca
psiklos <peter.siklos@vidux.net>
2020-12-14 17:20:06 +0100

    [#41] Added EqualsAndHashCode and ToString to RawCameraData

--- backend/src/main/java/net/vidux/camhub/discovery/RawCameraData.java
+++ backend/src/main/java/net/vidux/camhub/discovery/RawCameraData.java
@@ -1,9 +1,13 @@
 package net.vidux.camhub.discovery;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@EqualsAndHashCode
+@ToString
 @Builder
 class RawCameraData {
   String name;
