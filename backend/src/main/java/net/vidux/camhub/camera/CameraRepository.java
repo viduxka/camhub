2d360575af3b5f2bc34dd8222be33952afccffe8
psiklos <peter.siklos@vidux.net>
2020-12-15 07:45:24 +0100

    [#43] Updated repo to support search by serial number

--- backend/src/main/java/net/vidux/camhub/camera/CameraRepository.java
+++ backend/src/main/java/net/vidux/camhub/camera/CameraRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CameraRepository extends JpaRepository<Camera, String> {
+  Camera findBySerialNumber(String serialNumber);
 }
