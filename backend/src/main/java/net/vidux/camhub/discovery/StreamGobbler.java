c3967d0284b7ba938ddf04d272cb7d3c92b753e3
Gólity <patrik.golity@vidux.net>
2020-12-15 14:27:44 +0100

    [#42] Fixed encoding

--- backend/src/main/java/net/vidux/camhub/discovery/StreamGobbler.java
+++ backend/src/main/java/net/vidux/camhub/discovery/StreamGobbler.java
@@ -3,6 +3,7 @@ package net.vidux.camhub.discovery;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -17,8 +18,11 @@ class StreamGobbler implements Callable<List<String>> {
 
   @Override
   public List<String> call() {
-    List<String> list = new ArrayList<>();
-    new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(list::add);
+    ArrayList<String> list = new ArrayList<>();
+    new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+        .lines()
+        .filter(line -> !line.isBlank())
+        .forEach(list::add);
     return list;
   }
 }
