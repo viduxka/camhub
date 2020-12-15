e9b2e5bb4fd5bd079b0b993bd0fc9dac49818a00
psiklos <peter.siklos@vidux.net>
2020-12-15 07:47:43 +0100

    [#43] Added RawCameraDataProcessing

---
+++ backend/src/main/java/net/vidux/camhub/discovery/RawCameraDataProcessing.java
@@ -0,0 +1,28 @@
+package net.vidux.camhub.discovery;
+
+import net.vidux.camhub.camera.Camera;
+import net.vidux.camhub.camera.CameraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+class RawCameraDataProcessing {
+
+  @Autowired
+  CameraFactory cameraFactory;
+
+  @Autowired
+  CameraRepository cameraRepository;
+
+  void processRawCameraData(RawCameraData rawCameraData, Instant timestamp) {
+    Camera storedCamera = cameraRepository.findBySerialNumber(rawCameraData.getSerialNumber());
+    if (storedCamera == null) {
+      cameraRepository.save(cameraFactory.createCameraFromRawCameraDataAndTimeStamp(rawCameraData, timestamp));
+    } else {
+      storedCamera.setLastSeen(timestamp);
+      cameraRepository.save(storedCamera);
+    }
+  }
+}
