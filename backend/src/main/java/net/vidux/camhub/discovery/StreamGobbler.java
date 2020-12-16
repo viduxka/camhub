80a136677f1363bbcc6817d75d9376a4175cc8db
Gólity <patrik.golity@vidux.net>
2020-12-16 13:02:04 +0100

    [#42] Fixed list creation

--- backend/src/main/java/net/vidux/camhub/discovery/StreamGobbler.java
+++ backend/src/main/java/net/vidux/camhub/discovery/StreamGobbler.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
@@ -22,6 +21,6 @@ class StreamGobbler implements Callable<List<String>> {
     return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
         .lines()
         .filter(line -> !line.isBlank())
-        .collect(Collectors.toCollection(ArrayList::new));
+        .collect(Collectors.toList());
   }
 }
