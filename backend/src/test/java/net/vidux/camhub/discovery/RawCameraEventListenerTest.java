926c49d0c6392f505ce53f811e77c4a8bfea20eb
Gólity <patrik.golity@vidux.net>
2021-01-13 11:36:44 +0100

    [#64] Updated RawCameraEventListenerTest

--- backend/src/test/java/net/vidux/camhub/discovery/RawCameraEventListenerTest.java
+++ backend/src/test/java/net/vidux/camhub/discovery/RawCameraEventListenerTest.java
@@ -3,6 +3,7 @@ package net.vidux.camhub.discovery;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,21 +12,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class RawCameraEventListenerTest {
 
-  @Mock RawCameraEventListener listener;
-
-  @Mock RawCameraEventPublisher publisher;
+  @Mock RawCameraService service;
 
   @Test
   void testListener() {
     RawCameraData rawCameraData =
         new RawCameraData("VDX-OD-6MP-ML-IR-WDR", "94-e1-ac-d1-2a-44", "10.30.0.51", "C28752138");
 
-    publisher.publishRawCameraEvent(rawCameraData);
+    RawCameraEvent rawCameraEvent =
+        new RawCameraEvent(new RawCameraEventPublisher(), rawCameraData);
 
-    RawCameraEvent rawCameraEvent = new RawCameraEvent(publisher, rawCameraData);
+    new RawCameraEventListener(service).handleRawCameraEvent(rawCameraEvent);
 
-    listener.handleRawCameraEvent(rawCameraEvent);
-
-    verify(listener, times(1)).handleRawCameraEvent(rawCameraEvent);
+    verify(service, times(1))
+        .processRawCameraData(rawCameraData, Instant.ofEpochMilli(rawCameraEvent.getTimestamp()));
   }
 }
